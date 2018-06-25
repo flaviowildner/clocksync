039d2a268e7044905c01d37755f49e657d1e555f
flaviowildner <flaviowildner@hotmail.com>
2018-06-25 00:43:01 -0300

    Receiver node starts election if pid is greater

--- clocksync.py
+++ clocksync.py
@@ -31,13 +31,12 @@ class Packet:
 
 
 class Node:
-    currClock = 0
-    delay = 0
-    isCoord = False
-    ipCoord = ''
-    myAddr = ''
-    tempCoord = False
-    nodeList = []
+    currClock   = 0
+    delay       = 0
+    isCoord     = False
+    ip_coord    = ''
+    myAddr      = ''
+    nodeList    = []
 
     def clock(self):
         while True:
@@ -54,22 +53,26 @@ class Node:
             if os.getpid() > int(msg.content):
                 print('Enviando de volta pid', os.getpid())
                 sock.sendto(pickle.dumps(Packet(BULLY_ANSWER, os.getpid())), addr)
+                self.startBully(self.sock)
+                if self.isCoord == True:
+                    self.startBerkeley(self.sock)
         elif msg.type_msg == BULLY_ANSWER:
             print('Resposta da requisicao bully recebida com pid', int(msg.content), 'de', addr)
-            self.tempCoord = False
+            self.isCoord = False
             self.ip_coord = addr[0]
         elif msg.type_msg == BULLY_ANNOUNCE:
             print('Novo mestre: ', msg.content)
             self.ip_coord = msg.content
             self.isCoord = False
         elif msg.type_msg == BERKELEY_REQUEST:
+            print('Recebida requisicao de berkeley de relogio ', int(msg.content))
             diff = self.currClock - int(msg.content)
             print('Enviando diferenca do relogio', diff, 'Relogio:', self.currClock)
             msg = Packet(BERKELEY_RESPONSE, str(diff))
             sock.sendto(pickle.dumps(msg), addr)
         elif msg.type_msg == BERKELEY_RESPONSE:
-            self.nodeList.append((addr, msg.content))
             print('Recebida diferenca do relogio', int(msg.content), 'de', addr)
+            self.nodeList.append((addr, msg.content))
         elif msg.type_msg == BERKELEY_ADJUST:
             print('Recebido ajuste de relogio: ', int(msg.content))
             print('Alterando relogio de', self.currClock, 'para', self.currClock + int(msg.content))
@@ -86,17 +89,18 @@ class Node:
         msg = Packet(BULLY_REQUEST, pid)
         sock.sendto(pickle.dumps(msg), (MULTICAST_GROUP, PORT))
         print('Aguardando respostas...')
-        self.tempCoord = True
         remaining_time = time.time() + TIMEOUT
+        self.isCoord = True
         while True:
-            if(time.time() >= remaining_time):
+            systemTime = time.time()
+            if(systemTime >= remaining_time):
                 break
-            r, w, e = select.select([sock], [], [sock], remaining_time - time.time())
+            r, w, e = select.select([sock], [], [sock], remaining_time - systemTime)
             if not r:
                 break
-            for io in r:
-                self.handleUDPPacket(io)
-        if self.tempCoord == True:
+            else:
+                self.handleUDPPacket(sock)
+        if self.isCoord == True:
             print("Sou o novo coordenador!")
             self.annouceVictory(sock)
             self.isCoord = True
@@ -111,25 +115,28 @@ class Node:
         sock.sendto(pickle.dumps(msg), (MULTICAST_GROUP, PORT))
         remaining_time = time.time() + TIMEOUT
         while True:
-            if(time.time() >= remaining_time):
+            systemTime = time.time()
+            if(systemTime <= remaining_time):
+                r, w, e = select.select(
+                    [sock], [], [sock], remaining_time - systemTime)
+            else:
                 break
-            r, w, e = select.select([sock], [], [sock], remaining_time - time.time())
             if not r:
                 break
-            for io in r:
-                self.handleUDPPacket(io)
+            else:
+                self.handleUDPPacket(sock)
 
         print('Lista de escravos:')
-        media = 0.0
+        average = 0.0
         for addr, content in self.nodeList:
-            media += int(content)
-        media = int(media / (len(self.nodeList) + 1))
+            average += int(content)
+        average = int(average / (len(self.nodeList) + 1))
         for addr, content in self.nodeList:
-            msg = Packet(BERKELEY_ADJUST, str(media - int(content)))
-            print('Ajusta relogio de', addr, ':', 'em', media - int(content))
+            msg = Packet(BERKELEY_ADJUST, str(average - int(content)))
+            print('Ajusta relogio de', addr, ':', 'em', average - int(content))
             sock.sendto(pickle.dumps(msg), addr)
-        print('Alterando o relogio do mestre de', self.currClock, 'para', self.currClock + media)
-        self.currClock += media
+        print('Alterando o relogio do mestre de', self.currClock, 'para', self.currClock + average)
+        self.currClock += average
         self.nodeList.clear()
 
     def run(self):
@@ -181,6 +188,7 @@ class Node:
                     sys.stdin.readline()
                     if self.isCoord == True:
                         self.startBerkeley(self.sock)
+
 
 if __name__ == '__main__':
     node = Node()
